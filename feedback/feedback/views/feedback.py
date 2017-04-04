f651ecac88087f776b3bd9d49178d1c389f63266
antons <antons@ronis.mail>
2017-04-04 19:04:17 +0300

    update

--- feedback/feedback/views/feedback.py
+++ feedback/feedback/views/feedback.py
@@ -1,11 +1,11 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 from sqlalchemy.exc import DBAPIError
+import logging
 
 
 @view_config(route_name='feedback', renderer='../templates/feedback.mako')
 def my_view(request):
-
     validate_error = request.session.pop_flash()
     try:
         if request.session['feedback_data']:
@@ -15,5 +15,7 @@ def my_view(request):
             feedback_data = []
     except KeyError:
         feedback_data = []
+
+    logging.error('test')
 
     return {'errors': validate_error, 'data': feedback_data}
